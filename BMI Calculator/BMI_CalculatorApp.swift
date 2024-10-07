0c117567c04305061c7b54ed688e9871218e4495
Hüseyin Umut Kardaş <kardasumt@gmail.com>
2024-10-07 21:47:26 +0300

    Initial Commit

---
+++ BMI Calculator/BMI_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMI_CalculatorApp.swift
+//  BMI Calculator
+//
+//  Created by Hüseyin Umut Kardaş on 7.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct BMI_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
